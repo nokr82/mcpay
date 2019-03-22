95d0ea3def0c402f027308df72d38299836336e0
youngju <youngjucha0316@gmail.com>
2019-03-22 12:19:02 +0900

    동우등록

--- app/src/main/java/mc/pay/android/adapter/CouponAdapter.kt
+++ app/src/main/java/mc/pay/android/adapter/CouponAdapter.kt
@@ -3,44 +3,55 @@ package mc.pay.android.adapter
 import android.content.Context
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
-import org.json.JSONException
-import org.json.JSONObject
+import android.widget.ArrayAdapter
 
 
-open class CouponAdapter(context: Context, view: Int, data: Int): ArrayAdapter<JSONObject>(context, view, data) {
+
+
+open class CouponAdapter (context: Context, view:Int, data:Int) : ArrayAdapter<Int>(context, view, data) {
 
     private lateinit var item: ViewHolder
-    private var data:Int = data
-    private var view: Int = view
+    var view:Int = view
+    var data: Int = data
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var convertView = convertView
+
+    override fun getView(position: Int, convertView: View?, parent : ViewGroup?): View {
+
+        lateinit var retView: View
+
         if (convertView == null) {
-            convertView = View.inflate(context, view, null)
-            item = ViewHolder(convertView!!)
-            convertView!!.tag = item
+            retView = View.inflate(context, view, null)
+            item = ViewHolder(retView)
+            retView.tag = item
         } else {
+            retView = convertView
             item = convertView.tag as ViewHolder
             if (item == null) {
-                convertView = View.inflate(context, view, null)
-                item = ViewHolder(convertView!!)
-                convertView!!.tag = item
+                retView = View.inflate(context, view, null)
+                item = ViewHolder(retView)
+                retView.tag = item
             }
         }
 
-        try {
 
 
 
-        } catch (e: JSONException) {
-            e.printStackTrace()
-        }
-        return convertView
+        return retView
     }
 
+    override fun getItem(position: Int): Int {
+        return data
+    }
 
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return data
+    }
     class ViewHolder(v: View) {
+
 
 
         init {
@@ -48,5 +59,4 @@ open class CouponAdapter(context: Context, view: Int, data: Int): ArrayAdapter<J
 
         }
     }
-
 }
