222ed4d95c1e875e817b600a3f98ff1568a41e3a
youngju <youngjucha0316@gmail.com>
2019-03-22 16:33:17 +0900

    동우등록

--- app/src/main/java/mc/pay/android/activities/ManageActivity.kt
+++ app/src/main/java/mc/pay/android/activities/ManageActivity.kt
@@ -4,15 +4,23 @@ import android.app.ProgressDialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import com.loopj.android.http.JsonHttpResponseHandler
+import com.loopj.android.http.RequestParams
+import cz.msebera.android.httpclient.Header
 import mc.pay.android.R
 import kotlinx.android.synthetic.main.activity_manage.*
+import mc.pay.android.Actions.MemberAction
+import mc.pay.android.base.PrefUtils
 import mc.pay.android.base.RootActivity
+import mc.pay.android.base.Utils
+import org.json.JSONException
+import org.json.JSONObject
 
 class ManageActivity : RootActivity() {
 
     private lateinit var context: Context
     private var progressDialog: ProgressDialog? = null
-
+    var autoLogin = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manage)
@@ -20,6 +28,15 @@ class ManageActivity : RootActivity() {
         progressDialog = ProgressDialog(context, R.style.progressDialogTheme)
         progressDialog!!.setProgressStyle(android.R.style.Widget_DeviceDefault_Light_ProgressBar_Large)
         progressDialog!!.setCancelable(false)
+        loadInfo()
+        autoLogin = PrefUtils.getBooleanPreference(context, "autoLogin")
+
+        if (autoLogin){
+            autoIV.setImageResource(R.mipmap.check)
+        }else{
+            autoIV.setImageResource(0)
+        }
+
 
         backIV.setOnClickListener {
             finish()
@@ -30,7 +47,6 @@ class ManageActivity : RootActivity() {
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
         }
-
 
         franLL.setOnClickListener {
             var intent = Intent(context, FranchiseActivity::class.java)
@@ -53,9 +69,98 @@ class ManageActivity : RootActivity() {
             startActivity(intent)
         }
         autoIV.setOnClickListener {
-
+            if (autoLogin){
+                autoLogin = false
+                PrefUtils.setPreference(context, "autoLogin", autoLogin)
+                autoIV.setImageResource(0)
+            }else{
+                autoLogin = true
+                PrefUtils.setPreference(context, "autoLogin", autoLogin)
+                autoIV.setImageResource(R.mipmap.check)
+            }
         }
 
+    }
+
+    fun loadInfo() {
+        val params = RequestParams()
+        params.put("member_id", PrefUtils.getIntPreference(context, "member_id"))
+
+        MemberAction.my_info(params, object : JsonHttpResponseHandler() {
+
+            override fun onSuccess(statusCode: Int, headers: Array<Header>?, response: JSONObject?) {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+
+                try {
+                    val result = response!!.getString("result")
+
+                    print("result : $response")
+
+                    if ("ok" == result) {
+
+                        var member = response.getJSONObject("member")
+                        val name = Utils.getString(member,"name")
+                        nameTV.text = name
+
+
+
+                    } else {
+                    }
+
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Array<Header>?, responseString: String?) {
+
+                // System.out.println(responseString);
+            }
+
+            private fun error() {
+                Utils.alert(context, "조회중 장애가 발생하였습니다.")
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<Header>?,
+                responseString: String?,
+                throwable: Throwable
+            ) {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+
+                // System.out.println(responseString);
+
+                throwable.printStackTrace()
+                error()
+            }
+
+
+            override fun onStart() {
+                // show dialog
+                if (progressDialog != null) {
+
+                    progressDialog!!.show()
+                }
+            }
+
+            override fun onFinish() {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+            }
+        })
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        loadInfo()
     }
 
     override fun onDestroy() {
