6c2e451a6a8a18805d65287304b30ca3c7ebd58b
youngju <youngjucha0316@gmail.com>
2019-03-25 17:48:50 +0900

    동우등록

--- app/src/main/java/mc/pay/android/activities/FranchiseActivity.kt
+++ app/src/main/java/mc/pay/android/activities/FranchiseActivity.kt
@@ -1,13 +1,16 @@
 package mc.pay.android.activities
 
+import android.app.DatePickerDialog
 import android.app.ProgressDialog
 import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import com.loopj.android.http.JsonHttpResponseHandler
 import com.loopj.android.http.RequestParams
 import cz.msebera.android.httpclient.Header
 import mc.pay.android.R
 import kotlinx.android.synthetic.main.activity_franchisee.*
+import mc.pay.android.Actions.BankAction.pay_history
 import mc.pay.android.Actions.FranchiesAction
 import mc.pay.android.adapter.FranchiesAdapter
 import mc.pay.android.base.PrefUtils
@@ -24,6 +27,9 @@ class FranchiseActivity : RootActivity() {
     var adapterData:ArrayList<JSONObject> = ArrayList<JSONObject>()
     private lateinit var adapter: FranchiesAdapter
 
+    var first_day = ""
+    var last_day = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_franchisee)
@@ -38,10 +44,10 @@ class FranchiseActivity : RootActivity() {
 
 
         calLL.setOnClickListener {
-
+            datedlg()
         }
         cal2LL.setOnClickListener {
-
+            datedlg2()
         }
 
 
@@ -51,11 +57,47 @@ class FranchiseActivity : RootActivity() {
         index()
     }
 
+
+    fun datedlg() {
+        var day = Utils.todayStr()
+        var days =    day.split("-")
+        DatePickerDialog(context, dateSetListener, days[0].toInt(), days[1].toInt()-1, days[2].toInt()).show()
+    }
+
+    fun datedlg2() {
+        var day = Utils.todayStr()
+        var days =    day.split("-")
+        DatePickerDialog(context, dateSetListener2,days[0].toInt(), days[1].toInt(), days[2].toInt()).show()
+    }
+
+    private val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+        val msg = String.format("%d.%d.%d", year, monthOfYear + 1, dayOfMonth)
+
+        firstTV.text = msg
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+        first_day = msg
+        index()
+
+    }
+    private val dateSetListener2 = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+        val msg = String.format("%d.%d.%d", year, monthOfYear + 1, dayOfMonth)
+        val end_msg = String.format("%d-%d-%d", year, monthOfYear + 1, dayOfMonth)
+        endTV.text = msg
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+        last_day = msg
+        index()
+    }
+
+
+
     //은행정보보
     fun index() {
 
         val params = RequestParams()
         params.put("member_id", PrefUtils.getIntPreference(context,"member_id"))
+        params.put("first_day",first_day)
+        params.put("last_day",last_day)
+
         FranchiesAction.index(params, object : JsonHttpResponseHandler() {
 
             override fun onSuccess(statusCode: Int, headers: Array<Header>?, response: JSONObject?) {
